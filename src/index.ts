e6217e4557005070e8994a6ec9b51c8ad6300b1a
Austin Chabaud <austinc1023@yahoo.com>
2021-07-11 00:59:42 -0500

    added mikro

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('hello fsda')
+import { MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants"
+import { Post } from "./entities/Post"
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'mySocial',
+        type: 'postgresql',
+        debug: !__prod__,
+    })
+
+    const post = orm.em.create(Post, {title: 'my first post'})
+    await orm.em.persistAndFlush(post)
+    console.log('----------sql 2-------------')
+    await orm.em.nativeInsert(Post, {title: 'my second post'})
+}
+
+main()
